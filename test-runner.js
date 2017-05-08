f07e7c2d279ceee4408be6f827fe60898120371a
jogricn <nebojsajogric@gmail.com>
2017-05-08 05:06:02 +0200

    To be built with new buildpacks

--- test-runner.js
+++ test-runner.js
@@ -1,7 +1,7 @@
 var async = require("async");
 var fs = require('fs');
 var nodemailer = require("nodemailer");
-// var transport = nodemailer.createTransport("direct");
+//var transport = nodemailer.createTransport("direct"); - commented temporarily
 //var sys = require('util');
 var spawn = require('child_process').spawn;
 var parseArgs = require('minimist');
